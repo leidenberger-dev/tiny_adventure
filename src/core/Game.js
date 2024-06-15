ca87f53efc04fa74acdb05e778866921ef0d6f5d
Jörg Leidenberger <hello@joerg-leidenberger.com>
2024-06-15 19:58:14 +0200

    feat: Optimize image loading for better performance

--- src/core/Game.js
+++ src/core/Game.js
@@ -5,18 +5,40 @@ export class Game {
   constructor() {
     this.level = new Level1();
     this.renderer = new Renderer(this.level);
+    this.isLoaded = false;
+    this.loadLevel().then(() => {
+      this.isLoaded = true;
+      this.start();
+    });
   }
 
   start() {
-    setTimeout(() => {
-      this.gameLoop();
-    }, 1000);
+    this.gameLoop();
   }
 
   gameLoop = () => {
+    if (!this.isLoaded) return;
     this.renderer.draw();
     requestAnimationFrame(this.gameLoop);
   };
+
+  async loadLevel() {
+    // Erstelle eine Instanz des Levels, z.B. Level1
+    const level = this.level;
+
+    // Lade die JSON-Daten für die Karte
+    await level.mapData.loadJson();
+
+    // Initialisiere zusätzliche Level-spezifische Einstellungen oder Objekte
+    // Dies könnte beispielsweise das Setzen des Spielers an den Startpunkt umfassen
+    // oder das Initialisieren von Gegnern, falls erforderlich
+
+    // Setze das aktuelle Level im Spiel
+    // Dies könnte bedeuten, dass das Level-Objekt an die Game-Klasse übergeben wird
+    // oder dass es global oder in einem Zustandsmanager gespeichert wird
+
+    return level; // Rückgabe des geladenen Levels
+  }
 
   devMode() {
     this.level.setDevModeTrue();
