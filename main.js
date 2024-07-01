69a7dec75178a46b216a0d08c4aefe8332a55531
Jörg Leidenberger <hello@joerg-leidenberger.com>
2024-07-01 11:32:05 +0200

    chore: remove unused game start function

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ import { Game } from "./src/core/Game.js";
 document.addEventListener("DOMContentLoaded", () => {
   setupCanvas();
   const game = new Game();
-  game.start();
   window.devMode = game.devMode.bind(game);
 });
