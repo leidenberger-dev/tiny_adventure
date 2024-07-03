8df994a3f8eef0510862afe6a3fc40501f2edfde
Jörg Leidenberger <hello@joerg-leidenberger.com>
2024-07-03 08:23:49 +0200

    feat: Preload assets before initializing the game

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { setupCanvas } from "./src/config/canvas.js";
+import { preloadAssets } from "./src/utils/preloadAssets.js";
 import { Game } from "./src/core/Game.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   setupCanvas();
-  const game = new Game();
-  window.devMode = game.devMode.bind(game);
+  preloadAssets()
+    .then(() => {
+      console.log("Alle Assets wurden vorgeladen.");
+      const game = new Game();
+      window.devMode = game.devMode.bind(game);
+    })
+    .catch((error) => {
+      console.error("Fehler beim Vorladen der Assets", error);
+    });
 });
