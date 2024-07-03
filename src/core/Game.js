d47e9363271b6327a54673c1dc26065613b850cf
Jörg Leidenberger <hello@joerg-leidenberger.com>
2024-07-03 08:27:17 +0200

    feat: Improve game loop performance and loading flow

--- src/core/Game.js
+++ src/core/Game.js
@@ -5,60 +5,50 @@ import { Renderer } from "./Renderer.js";
 import { pressedKeys } from "../config/keys.js";
 import { handleInput } from "./inputHandler.js";
 import { playSound } from "../utils/playSound.js";
+import { preloadAssets } from "../utils/preloadAssets.js";
 
 export let devMode = false;
 
 export class Game {
   static isMuted = false;
   constructor() {
-    this.musicSound = null;
-    this.worldSound = null;
+    this.isLoadingLevel = true;
+    this.lastFrameTime = 0;
+    this.targetFrameTime = 1000 / 60;
     this.checkDevMode();
     this.input = handleInput;
     this.input();
     this.level = new Level1();
     this.renderer = new Renderer(this.level);
-    this.isLoaded = false;
-    this.gameState = "loading";
-    this.gameLoopId = null;
-    this.loadLevel().then(() => {
-      this.isLoaded = true;
-      this.gameState = "running";
-      this.start();
-    });
     this.playSound = playSound;
     this.soundsPlaying = {};
+    this.loadLevel().then(() => {
+      this.isLoadingLevel = false;
+      this.start();
+    });
   }
 
   start() {
-    if (this.isLoaded) {
-      if (!this.gameLoopId) {
-        this.gameLoopId = requestAnimationFrame(this.gameLoop);
-      }
-    }
+    this.gameLoop();
   }
 
-  gameLoop = () => {
-    if (this.gameState !== "running") {
-      cancelAnimationFrame(this.gameLoopId);
-      this.gameLoopId = null;
-      return;
-    }
-    this.handleLevelReset();
-    this.handleMusic();
-    this.handlePause();
-    this.updateVolume();
-    this.renderer.draw();
-    this.handleNextLevel();
-    this.gameLoopId = requestAnimationFrame(this.gameLoop);
-  };
+  gameLoop(timestamp) {
+    const deltaTime = timestamp - this.lastFrameTime;
 
-  async loadLevel() {
-    this.gameState = "loading";
-    const level = this.level;
-    await level.mapData.loadJson();
-    this.gameState = "running";
-    return level;
+    if (deltaTime >= this.targetFrameTime) {
+      this.lastFrameTime = timestamp - (deltaTime % this.targetFrameTime);
+
+      if (!this.isLoadingLevel) {
+        this.handleLevelReset();
+        this.handleMusic();
+        this.handlePause();
+        this.updateVolume();
+        this.renderer.draw();
+        this.handleNextLevel();
+      }
+    }
+
+    requestAnimationFrame(this.gameLoop.bind(this));
   }
 
   devMode() {
@@ -83,17 +73,23 @@ export class Game {
     }
   }
 
+  async loadLevel() {
+    await this.level.mapData.loadJson();
+  }
+
   async handleNextLevel() {
+    if (this.isLoadingLevel) return;
     if (!this.level.door) return;
     const guiSettings = this.renderer.gui;
     const levels = [Level1, Level2, Level3];
     if (this.level.door.doorOpen) {
+      this.isLoadingLevel = true;
+      await preloadAssets();
       this.level = new levels[this.level.levelNumber]();
       this.renderer = new Renderer(this.level, guiSettings);
       this.renderer.gui.pointsbar.column = 0;
-      this.isLoaded = false;
       await this.loadLevel().then(() => {
-        this.isLoaded = true;
+        this.isLoadingLevel = false;
         this.start();
       });
     }
@@ -124,7 +120,6 @@ export class Game {
       this.isMusicPlaying = true;
       this.musicSound = this.playSound("./assets/sounds/music.mp3", 0.7, 1);
       this.musicSound.loop = true;
-      console.log(this.musicSound);
       this.worldSound = this.playSound("./assets/sounds/world.wav", 0.5, 1);
       this.worldSound.loop = true;
     }
